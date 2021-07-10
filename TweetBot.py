70c453bb1505a6e1223815940bf13cdaf21d3c7b
Andriani <theblue_squirrel@yahoo.com>
2021-07-10 05:25:55 -0400

    fixed output to file

--- TweetBot.py
+++ TweetBot.py
@@ -36,7 +36,7 @@ for tweet in tweepy.Cursor(api.user_timeline, chloe, since=latestTweetID).items(
             tweet.favorite()
             api.update_status(status='If you get this that means the bot i created is working... How long did it take? It doesnt matter, I wont miss a post!',
                               in_reply_to_status_id=latestTweetID, auto_populate_reply_metadata=True)
-            MakeFile('LatestTweetID.py', 'latestTweetChloe = '+tweet.id_str)
+            MakeFile('LatestTweetID.py', 'latestTweet = '+tweet.id_str)
             print("Current ID is larger: " + tweet.id_str)
             print("\nOld tweet (in file): "+str(latestTweetID))
             sleep(2)
