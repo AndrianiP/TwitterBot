5e490e8356e4deb8aff913a0849d03fa320a8ca6
Andriani <theblue_squirrel@yahoo.com>
2021-07-10 05:16:39 -0400

    Changed Reply messsage

--- TweetBot.py
+++ TweetBot.py
@@ -18,9 +18,8 @@ latestTweetID = since_id = latestTweet
 #latestTweetIDZay = since_id = latestTweetZay
 
 filepath = os.getcwd()
-
 def MakeFile(file_name, newID):
-    temp_path = filepath +file_name
+    temp_path = filepath + file_name
     with open(file_name, 'w') as f:
         f.write(
             newID
@@ -28,19 +27,20 @@ def MakeFile(file_name, newID):
 
 for tweet in tweepy.Cursor(api.user_timeline, chloe, since=latestTweetID).items():
     tempID = tweet.id
-    try:      
+    try:
         if(tempID > latestTweetID):
             latestTweetID = tempID
             print("\n\n")
             print(tweet.text)
             print(tweet.id)
             tweet.favorite()
-            api.update_status(status = 'Have you seen the killer bean movie?', in_reply_to_status_id = latestTweetID , auto_populate_reply_metadata=True)
-            MakeFile('LatestTweetID.py','latestTweetChloe = '+tweet.id_str)
-            print("Current ID is larger: " +tweet.id_str)
+            api.update_status(status='If you get this that means the bot i created is working... How long did it take? It doesnt matter, I wont miss a post!',
+                              in_reply_to_status_id=latestTweetID, auto_populate_reply_metadata=True)
+            MakeFile('LatestTweetID.py', 'latestTweetChloe = '+tweet.id_str)
+            print("Current ID is larger: " + tweet.id_str)
             print("\nOld tweet (in file): "+str(latestTweetID))
+            sleep(2)
     except Exception as e:
         print(e)
     print("NO TWEETS WE ARE SLEEPING")
     time.sleep(120)
-    
